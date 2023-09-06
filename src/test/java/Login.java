92935c8b666d79f4d9d65a6e09cda2ad6f9c3b56
Blessing Edidiong Johnson <blessingedidiongjohnson@gmail.com>
2023-09-06 15:51:57 +0100

    Pushing to Github

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -1,3 +1,21 @@
-public class Login {
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 
+
+public class Login {
+    WebDriver driver;
+    @BeforeClass void setUp() {
+        ChromeOptions option = new ChromeOptions();
+        option.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(option);
+    }
+    @BeforeMethod
+    void  reloadPage(){
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+    }
+
+    
 }
